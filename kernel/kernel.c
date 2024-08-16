7dd6f5052fea44ce8abe8ee101383d3e3527147f
ShonGutman <gshon2007@gmail.com>
2024-08-16 17:56:11 +0300

    modifed kernel a bit

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,8 +1,20 @@
 
 #define VIDEO_MEMORY 0xb8000
+#define WHITE_ON_BLACK 0x0f
+
+#include "../drivers/screen.h"
 
 void main() 
 {
     char* video_memory = (char*) VIDEO_MEMORY;
-    *video_memory = 'X';
+    
+    uint32 offset = get_cursor_offset();
+
+    char* osName = "TSOS";
+
+    for(int i = 0; i < 4; i++)
+    {
+        video_memory[offset + 2 * i] = osName[i];
+        video_memory[offset + 1 + (2 * i)] = WHITE_ON_BLACK;
+    }
 }
