cf9bccb72628557ecc7bfd4400abd912f9ab439c
ShonGutman <gshon2007@gmail.com>
2024-08-18 16:01:25 +0300

    small test in kernel.c - it works

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,20 +1,13 @@
 
-#define VIDEO_MEMORY 0xb8000
-#define WHITE_ON_BLACK 0x0f
 
 #include "../drivers/screen.h"
 
 void main() 
 {
-    char* video_memory = (char*) VIDEO_MEMORY;
-    
-    uint32 offset = get_cursor_offset();
 
-    char* osName = "TSOS";
+    char* osName = "TSOS\nBy - Shon and Tedy\n\n\n\n\n\ntest :)";
 
-    for(int i = 0; i < 4; i++)
-    {
-        video_memory[offset + 2 * i] = osName[i];
-        video_memory[offset + 1 + (2 * i)] = WHITE_ON_BLACK;
-    }
+    clear_screen();
+
+    print(osName);
 }
