fec7be55b9c37f037a65a88ef95b540b48301e52
ShonGutman <gshon2007@gmail.com>
2024-08-18 17:59:57 +0300

    returned kernel to print project name and our name

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -5,12 +5,9 @@
 void main() 
 {
 
-    char* osName = "TSOS\nBy - Shon and Tedy\nproject";
-    char* numbers = "\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n14\n15\n16\n17\n18\n19\n20\n21\n22test me";
+    char* osName = "TSOS\nBy - Shon and Tedy\n";
 
     clear_screen();
 
     print(osName);
-    //we will see that TSOS was removed
-    print(numbers);
 }
