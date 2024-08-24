dc9efa0225c2356a3ed20af827c47406faa15d92
ShonGutman <gshon2007@gmail.com>
2024-08-24 15:09:34 +0300

    added extren decleration of interrupt functions

--- cpu/idt.c
+++ cpu/idt.c
@@ -2,7 +2,7 @@
 
 #define IDT_ENTRIES 256
 
-#define GDT_SEGMENT_SELECTOR 0b00001000
+#define GDT_CODE_SEGMENT_SELECTOR 0b00001000
 #define FLAGS 0b10001110
 
 idt_entry_struct idt_entries[IDT_ENTRIES];
@@ -13,7 +13,7 @@ void set_idt_entry(const uint8 num, const uint32 offset)
     idt_entries[num].offset_low = offset & 0xffff;
     idt_entries[num].offset_high = (offset >> 16) & 0xffff;
     idt_entries[num].always0 = 0;
-    idt_entries[num].segmentSelector = GDT_SEGMENT_SELECTOR;
+    idt_entries[num].segmentSelector = GDT_CODE_SEGMENT_SELECTOR;
     idt_entries[num].flags = FLAGS;
 }
 
--- cpu/idt.h
+++ cpu/idt.h
@@ -2,6 +2,7 @@
 #define IDT_H
 
 #include "../headers/types.h"
+#include "../libc/memory.h"
 
 typedef struct
 {
@@ -44,6 +45,99 @@ void set_idt_entry(const uint8 num, const uint32 offset);
  * and the offset is set to the address of the IDT entries array.
  */
 void idt_init();
+
+
+
+
+/**
+ * ISR - Interrupt Service Routines
+ * these are the function that are executed when an interrupt oocurs due to cpu exceptions
+ */
+
+
+/**This interrupt is triggered when the processor attempts to divide a number by zero */
+extern void isr0(); /*Divide by zero*/
+
+/**This interrupt is used for debugging purposes. 
+ * It can be triggered intentionally by setting a breakpoint in the code or by hardware-based debugging tools. */
+extern void isr1(); /*Debug exception*/
+
+/**This interrupt is a high-priority interrupt that cannot be masked or ignored. 
+ * It is typically used for critical hardware events like memory parity errors or hardware failures. */
+extern void isr2(); /*Non-maskable interrupt (NMI)*/
+
+/**this interrupt can be used for debugging purposes. 
+ * It can be triggered by setting a breakpoint in the code. */
+extern void isr3(); /*Breakpoint*/
+
+/**his interrupt is triggered when an arithmetic operation results in an overflow, 
+ * meaning the result is too large to be represented in the current data type. */
+extern void isr4(); /*Overflow*/
+
+/**This interrupt is triggered when a value is outside the specified bounds. */
+extern void isr5(); /*Out of bounds*/
+
+/**This interrupt is triggered when the processor encounters an invalid or unrecognized instruction. */
+extern void isr6(); /*Invalid opcode*/
+
+/**his interrupt is triggered when a device is not available or ready for use. */
+extern void isr7(); /*Device not available*/
+
+/**This interrupt is a critical exception that occurs 
+ * when another exception happens while the processor is already handling an exception. */
+extern void isr8(); /*Double fault*/
+
+/**This interrupt is related to the floating-point coprocessor (FPU) 
+ * and is triggered when the FPU attempts to access a segment that is not present or has incorrect permissions. */
+extern void isr9(); /*Coprocessor segment overrun*/
+
+/**This interrupt is triggered when the processor encounters an invalid Task State Segment (TSS), 
+ * which is used for task switching. */
+extern void isr10(); /*Invalid TSS*/
+
+/**This interrupt is triggered when the processor attempts to access a memory segment that is not present in the memory management unit (MMU). */
+extern void isr11(); /*Segment not present*/
+
+/**This interrupt is triggered when the processor encounters an error related to the stack segment, 
+ * such as a stack overflow or underflow. */
+extern void isr12(); /*Stack segment fault*/
+
+/**This is a general-purpose exception that can be triggered by various errors, 
+ * such as invalid memory accesses, privilege violations, or incorrect use of system resources. */
+extern void isr13(); /*General protection fault*/
+
+/**This interrupt is triggered when the processor encounters a page fault, 
+ * which occurs when a memory access attempts to reference a page that is not present in physical memory. */
+extern void isr14(); /*Page fault*/
+
+
+extern void isr15(); /*Reserved*/
+
+/**This interrupt is triggered when the FPU encounters an error during floating-point operations */
+extern void isr16(); /*Floating-point error*/
+
+/**This interrupt is triggered when the processor detects an alignment error, 
+ * which occurs when data is not accessed on the correct memory boundary. */
+extern void isr17(); /*Alignment check*/
+
+/**This interrupt is a critical exception that indicates a severe hardware error,
+ * such as a processor or memory failure. */
+extern void isr18(); /*Machine check*/
+
+/*Reserved*/
+extern void isr19();
+extern void isr20();
+extern void isr21();
+extern void isr22();
+extern void isr23();
+extern void isr24();
+extern void isr25();
+extern void isr26();
+extern void isr27();
+extern void isr28();
+extern void isr29();
+extern void isr30();
+extern void isr31();
 
 
 #endif
