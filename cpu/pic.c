0be8c45f5455b2fdeb67da04648a872987e491b5
ShonGutman <gshon2007@gmail.com>
2024-08-25 03:17:42 +0300

    added pic files with remap

--- cpu/pic.c
+++ cpu/pic.c
@@ -6,14 +6,75 @@
 #define SLAVE_PIC_COMMAND_PORT 0xa0
 #define SLAVE_PIC_DATA_PORT 0xa1
 
+
+/**********************************************************
+ * Private kernel functions signiture                     *
+ **********************************************************/
+
 void pic_remap(const uint32 master_offset, const uint32 slave_offset);
+
+
+/**********************************************************
+ * Public Kernel API functions                            *
+ **********************************************************/
 
 void pic_init()
 {
-
+    /**
+     * When the CPU boots, the PIC maps:
+     * - IRQs 0-7 to interrupts 8-15.
+     * - IRQs 8-15 to interrupts 112-119.
+     * 
+     * This causes conflicts with the CPU's ISRs (Interrupt Service Routines) which are mapped 
+     * to interrupts 0-31. To avoid this, we remap the IRQs:
+     * - IRQs 0-7 are remapped to interrupts 32-39.
+     * - IRQs 8-15 are remapped to interrupts 40-47.
+     */
+    pic_remap(32, 40);
 }
 
+
+/**********************************************************
+ * Private kernel functions                               *
+ **********************************************************/
+
+/*
+ * Remaps the master and slave PIC (Programmable Interrupt Controller) to new interrupt offsets.
+ * This is done to ensure that the IRQs do not overlap with the CPU's reserved interrupts (0-31).
+ *
+ * @param master_offset The new interrupt offset for the master PIC.
+ * @param slave_offset  The new interrupt offset for the slave PIC.
+ */
 void pic_remap(const uint32 master_offset, const uint32 slave_offset)
 {
+    //send initalization sequence to the master command PIC and slave command PIC
+    port_byte_out(MASTER_PIC_COMMAND_PORT, 0x11);
+    port_byte_out(SLAVE_PIC_COMMAND_PORT, 0x11);
 
+    //set master PIC offset
+    port_byte_out(MASTER_PIC_DATA_PORT, master_offset);
+
+    //set slave PIC offset
+    port_byte_out(SLAVE_PIC_DATA_PORT, slave_offset);
+
+    /**
+     * Tell Master PIC that there is a slave PIC at IRQ2 (4 = 0000 0100)
+     * 
+     * Why we send 4?
+     * each bit represnt one of the IRQs
+     * most left bit represnt IRQ7 and most right bit represent IRQ0
+     *     
+    */
+    port_byte_out(MASTER_PIC_DATA_PORT, 0x04);
+
+    //Tell slave PIC about this role
+    port_byte_out(SLAVE_PIC_DATA_PORT, 0x02);
+
+    //we are in 8086 mode
+    port_byte_out(MASTER_PIC_DATA_PORT, 0x01);
+    port_byte_out(SLAVE_PIC_DATA_PORT, 0x01);
+
+    //enable all IRQs
+    port_byte_out(MASTER_PIC_DATA_PORT, 0);
+    port_byte_out(SLAVE_PIC_DATA_PORT, 0);
 }
--- cpu/pic.h
+++ cpu/pic.h
@@ -4,6 +4,28 @@
 #include "../headers/types.h"
 #include "../drivers/ports.h"
 
+/**
+ * Source: https://wiki.osdev.org/8259_PIC#Programming_the_PIC_chips
+ * 
+ * What is PIC:
+ * - PIC stands for Programmable Interrupt Controller.
+ * - It is a critical component in the x86 architecture, enabling the system to be interrupt-driven.
+ * 
+ * Why PIC is important:
+ * - Without a PIC, the system would need to constantly poll devices to check for events, leading to inefficiency.
+ * - The PIC allows devices to signal the CPU only when they need attention, improving overall system performance.
+ * 
+ * Note:
+ * - The Slave PIC is typically connected to the Master PIC via IRQ2.
+ */
+
+
+/*
+ * Initializes the Programmable Interrupt Controller (PIC) by remapping the IRQs to avoid conflicts
+ * with the CPU's existing interrupt vector table. The IRQs are remapped so that:
+ * - IRQs 0-7 are mapped to interrupts 32-39.
+ * - IRQs 8-15 are mapped to interrupts 40-47.
+ */
 void pic_init();
 
 #endif
