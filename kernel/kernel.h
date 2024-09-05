46e19fec5e27906dd0779fdc65ab57ff1ae22468
Tedy2606 <tedkoud@gmail.com>
2024-09-06 02:11:46 +0300

    added documentation

---
+++ kernel/kernel.h
@@ -0,0 +1,12 @@
+#ifndef KERNEL_H
+#define KERNEL_H
+
+/*
+ * procces the user input so if the input is END the os will shut down
+ *
+ * @param input The input of the user 
+ */
+void procces_user_input(const char* input);
+
+
+#endif
