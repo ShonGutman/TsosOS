efc8d45221b27956fb831a7749d38fbd9c34868e
ShonGutman <gshon2007@gmail.com>
2024-10-15 23:38:55 +0300

    removed function from this file to reduce space

--- boot/print.asm
+++ boot/print.asm
@@ -25,31 +25,4 @@ print_loop:
 
 print_done:
     popa                 ; Restore all registers from the stack
-    ret                  
-
-
-;-----------------------------------------------------------------------
-; Function: print_nl
-; Purpose:  Prints a newline to the screen.
-;
-; Parameters:
-;   None
-;
-; Return:
-;   None
-;-----------------------------------------------------------------------
-
-print_nl:
-    pusha                ; Save all registers onto the stack
-    
-    ; BIOS interrupt to print newline (line feed + carriage return)
-
-    mov ah, 0x0e         
-    mov al, 0x0a         ; Newline character (line feed)
-    int 0x10             
-
-    mov al, 0x0d         ; Carriage return
-    int 0x10             
-    
-    popa                 ; Restore all registers from the stack
-    ret                  
+    ret                                
